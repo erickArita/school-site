9fdbbd88261856b9658fc450ceb6aacc91aa5c58
erickArita <erick.emao@gmail.com>
2020-06-12 17:09:01 -0600

    mapa

--- js/main.js
+++ js/main.js
@@ -1,13 +1,2 @@
 AOS.init();
-// esto le da informacion a google maps 
-function iniciarMap() {
-    let coord = { lat: 14.8698517, lng: -89.0907986 };
-    let map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 2,
-        center: coord
-    });
-    let marker = new google.maps.Marker({
-        position:coord,
-        map:map
-    })
-}
+ 
