a1ba7abfd00ea92571f4ee9b7c9e6f3739721b0c
erickArita <erick.emao@gmail.com>
2020-06-15 13:58:54 -0600

    nosotros

--- js/main.js
+++ js/main.js
@@ -19,30 +19,50 @@ let ultimoScroll = 0;
 $(window).scroll(() => {
     if ($("#nav").offset().top > 550) {
         $("#nav").addClass("colorBg") && $("#nav").removeClass("bg-light") &&
-            $('#ul').addClass('fontC')
-
+            $('#ul').addClass('fontC') 
+            $("#logoI").css('background-color','#fff')
+            $("#logoI").css('border-radius','400px')
     } else {
         $("#nav").removeClass("colorBg") && $("#nav").addClass("bg-light") && $('#ul').removeClass('fontC')
     }
-    // esta variable se llena con el estado actual de la  distancia superior
+    // esta variable se llena con el estado actual de la nav a la distancia superior
     let st = window.pageYOffset;
-
+    // si el estado actual es mayor que la ultima posicion de la nav
     if (st > ultimoScroll) {
         console.log('para abajo');
-        nav.style.cssText= 'opacity:0;transition:800ms';
-        
+        nav.style.cssText = 'opacity:0;transition:800ms';
+
     } else {
         console.log('para arriba');
 
-        nav.style.cssText= 'opacity:100;transition:800ms';
-     }
+        nav.style.cssText = 'opacity:100;transition:800ms';
+    }
     ultimoScroll = st;
 })
 
+// seleccionar todos los div con esa clase
+const card = document.querySelectorAll('.card')
+
+// recorro todos los div con esa clase
+card.forEach(x => {
+    // quiero saber si el mouse está encima de alguna card para ponerle o quitarle clases
+    x.addEventListener('mouseover', (e) => {
 
 
+        const div = x.querySelector('.m')
+        div.classList.add('mAnimated')
+         
+    })
 
-const card =document.querySelectorAll('.card')
+    // se quitan las clases que se le agregaron
+    x.addEventListener('mouseout', (e) => {
+
+
+        const div = x.querySelector('.m')
+        div.classList.remove('mAnimated')
+
+    })
+
+})
 
  
-
